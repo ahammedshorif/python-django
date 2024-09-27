daa545e5d439492f9ba297d628f4edf85ad385cd
ahammedshorf <ahammedshorif20@gmail.com>
2024-09-28 01:44:57 +0600

    c string

--- day2/string.py
+++ day2/string.py
@@ -2,3 +2,8 @@ str1 = "say hello to Python"
 print(str1) 
 print(str1[4:9]) # print 4 index to before 9 index char
 print(str1[4:]) # print 4 index to last index char
+
+# string functions
+
+print(str1.capitalize())
+print(str1.upper())
